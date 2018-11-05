12bb8243436a6fd1cb2ad475a7f44e7984f3c108
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2018-11-05 21:59:14 +0100

    Made a basic dark mode, sticky navbar and modal popup effect#

--- public/index.js
+++ public/index.js
@@ -1,9 +1,39 @@
-const body = document.querySelector('body');
-const button = document.querySelector('button')
+//Dark Mode Function
 
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+const modal = document.querySelector('.modal');
+const modalBackground = document.querySelector('.modal-background');
 
 
 
 button.addEventListener('click', () => {
   body.classList.toggle('dark-mode'); //byter från lägga till, till ta bort.
+
 });
+
+
+let navbar = document.querySelector('.navbar');
+let sticky = navbar.offsetTop;
+
+//NavBar sticking to the top.
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+
+const openModal = () => {
+  modal.style.display = 'block';
+  modalBackground.style.display = 'block';
+};
+modalBackground.addEventListener('click', () => {
+  modal.style.display = 'none';
+  modalBackground.style.display = 'none';
+});
+
+
+setTimeout(openModal, 3000);
