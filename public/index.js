e30b65f21852d174d08812d8fa2dd44a2e575806
Maja Filipsson <majafilipsson@gmail.com>
2018-11-07 15:53:08 +0100

    Made navbar responsive

--- public/index.js
+++ public/index.js
@@ -49,3 +49,15 @@ modalBackground.addEventListener('click', () => {
 
 
 setTimeout(openModal, 3000);
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
