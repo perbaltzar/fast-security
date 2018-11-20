bf43fffb77aadfa27a3ef284e3d1c5f4715dd5dc
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2018-11-20 22:14:36 +0100

    made a new index and style file to do the page design in, alsoo made that design

---
+++ public/newIndex.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="newStyle.css">
+  <title></title>
+
+</head>
+<body>
+  <header class="main-navbar">
+    <div class="navbar-container">
+      <img class="navbar-logo" src="att/saab-logo.png"/>
+      <h2 class="contact nav-item">Contact</h2>
+      <h2 class="newsletter nav-item">Newsletter</h2>
+      <h2 class="brochure nav-item">Brochure</h2>
+      <h2 class="about nav-item">About</h2>
+      <div class="burger-container">
+        <div class="burger burger-1"></div>
+        <div class="burger burger-2"></div>
+        <div class="burger burger-3"></div>
+      </div><!--/Burger Container-->
+
+    </div>
+  </header>
+
+</div>
+<div class="hero first-hero">
+  <div class="hero-headline">
+    <h5>Join the future</h5>
+    <p>FAST SECURITY 1.0</p>
+  </div>
+</div>
+<div class="between-div">
+  <p>Many people still think Saab makes cars, and that is correct. </p>
+  <h5>We are on the go for the future.</h5>
+  <p>Saab is very much alive and well, as one of the world’s most<br>
+    innovative companies. And now we are proud to present our <br>
+    motorcycle for the future: FastSecurity, FS.</p>
+
+  </div>
+  <div class="hero second-hero">
+
+  </div>
+  <div class="two-pictures">
+    <div class="side-by-side left-image">
+
+    </div>
+    <div class="side-by-side right-image">
+
+    </div>
+  </div>
+  <div class="between-div">
+    <p>Saab started life in 1937 in Trollhättan, Sweden,
+      as an aeroplane manufacturer to provide aircraft for the Swedish Air Force,
+      just as Europe was steeling itself for another war.
+      The company's full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab.
+      The head office soon moved to the Swedish university town of Linköping,
+      where this massive company has its largest operation today.
+      Today we are starting a new chapter. Fast Security, FS. Is the future car.</p>
+    </div>
+    <div class="hero third-hero">
+    </div>
+    <div class="footer">
+      <div class="newsletter-brochure">
+        <p>GET THE LATEST NEWS</p>
+        <hr>
+        <div class="inputfield">
+          <input class="email-input" type="email" name="" value="">
+          <button class="newsletter" type="button" name="button"><img src="att/button-arrow.svg"></button><br>
+        </div>
+        <button class="brochure" type="button" name="button">DOWNLOAD OUR BROCHURE</button>
+      </div>
+      <div class="footer-logo">
+        <img src="att/saab-logo.png">
+      </div>
+      <div class="footer-buttons">
+        <div class="footer-contact footer-button">
+          CONTACT
+        </div>
+        <hr>
+        <div class="footer-newsletter line-left footer-button">
+          NEWSLETTER
+        </div>
+        <hr>
+        <div class="footer-brochure line-left footer-button">
+          BROCHURE
+        </div>
+        <hr>
+        <div class="footer-about line-left footer-button">
+          ABOUT
+        </div>
+      </div>
+      <div class="footer-line">
+        <hr>
+      </div>
+      <div class="social-media">
+        <img src="att/facebook-icon.svg">
+        <img src="att/linkedin-icon.svg">
+        <img src="att/twitter-icon.svg">
+      </div>
+      <div class="footer-copywrite">
+        © 2019 FastSecurity
+
+      </div>
+    </div>
+  </body>
+  </html>
